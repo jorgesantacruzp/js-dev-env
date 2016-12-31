c4489eda08bf217bf14439c6338e910a868f1e76
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2016-12-30 21:36:42 -0500

    Mock data is served via JSON Server

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ getUsers().then(result => {
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
       </tr>`
   });
 
