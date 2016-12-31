fd15044f68838f82ab379871e952c63362364fba
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2016-12-30 19:49:28 -0500

    HTTP call with fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1, "firstName": "Bob"},
+    {"id": 2, "firstName": "Tammy"},
+    {"id": 3, "firstName": "Tina"}
+    ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-/* eslint-disable no-console */
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-//debugger;
-console.log(`I would pay ${courseValue} for this!`);
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
