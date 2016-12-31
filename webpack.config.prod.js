42917b9d36519764584ab00daa51562e157e9ca9
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2016-12-31 10:52:11 -0500

    Webpack generates dynamic html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,16 +16,23 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    // inject tells webpack to inject any necessary script tag.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
+
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
 
     // Minify JS
     new webpack.optimize.UglifyJsPlugin()
   ],
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style', 'css']}
-    ]
-  }
+module: {
+  loaders: [
+    {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+    {test: /\.css$/, loaders: ['style', 'css']}
+  ]
+}
 }
